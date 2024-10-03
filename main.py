332c6c3cc0fab86d4b7750f8dd056ad82dde6259
martinSredkov <ash999@abv.bg>
2024-10-03 11:42:29 +0300

    Add more players, fix prints, optimize player logic

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
+from colorama import Fore, Back, Style
+
+
+# prints the field in the current state
 def print_matrix(matrix):
     for line in matrix:
-        print(line)
+        print(*line)
 
-def is_move_valid(move, size):
-    input_row, input_col = int(move.split(" ")[0]), int(move.split(" ")[1])
-    if not 0 < input_row <= size or not 0 < input_col <= size:
+# checks if move is valid - in range, not occupied
+def is_move_valid(move, size, current_state_field):
+    try:
+        input_row, input_col = move.split(" ")
+        if input_row.isdigit() and input_col.isdigit():
+            input_row = int(input_row) - 1
+            input_col = int(input_col) - 1
+            return 0 <= input_row <= size and 0 <= input_col <= size and current_state_field[input_row][input_col] == "-"
+    except ValueError:
         return False
-    return True
 
-def is_result_draw(matrix):
+# checks if result is draw by searching for unoccupied spaces
+def is_field_full(matrix):
     for line in matrix:
         for element in line:
-            if element == "*":
+            if element == "-":
                 return False
     return True
 
+# universal win condition check
+def win_check(lst):
+    if len(set(lst)) == 1 and "-" not in lst:
+        return  True
+
+# adds elements of rows, columns and diagonals to separate lists and passes them to win_check function
 def is_player_winner(matrix):
     result = []
     matrix_size = len(matrix)
-    def win_check(lst):
-        if len(set(lst)) == 1 and "*" not in lst:
-            return  True
-    # horizontal check
+
+    # checks if winning conditions are met in rows
     for line in matrix:
         if win_check(line):
             return True
-    #vertical check
+    # checks if winning conditions are met in columns
     for k in range(matrix_size):
         for j in range(matrix_size):
             result.append(matrix[j][k])
@@ -35,78 +49,73 @@ def is_player_winner(matrix):
 
     if win_check(result):
         return True
-    #diagonal check left to right
+    # checks if winning conditions are met in left to right diagonal
     for l in range(matrix_size):
         result.append(matrix[l][l])
     if win_check(result):
         return True
     result = []
-    #diagonal check right to left
+    # checks if winning conditions are met in right to left diagonal
     for m in range(matrix_size, 0, - 1):
         result.append(matrix[matrix_size - m][m - 1])
     if win_check(result):
         return True
-
-field = []
-field_size = input("Please enter field size:\n")
-#field size validation
-is_size_valid = False
-while not is_size_valid:
-    try:
-        field_size = int(field_size)
-        if field_size > 1:
-            break
-        else:
-            field_size = input("Please enter valid field size:\n")
-    except ValueError:
-        field_size = input("Please enter valid field size:\n")
-
-
-
-
-for i in range(field_size):
-    field.append([])
-    for _ in range(field_size):
-        field[i].append("*")
-
-while True:
-    player_one_turn = True
-    while player_one_turn:
+# starts the game
+if __name__ == "__main__":
+    current_player = 0
+    player_markers = []
+    field = []
+    # User input for number of players and validation of given input
+    total_players = input("Please, enter number of players:\n")
+    while not total_players.isdigit():
+        total_players = input(Fore.RED + "Enter valid number of players:\n")
+        print(Style.RESET_ALL)
+    while not 1 < int(total_players):
+        total_players = input(Fore.RED + "At least two players are needed to play the game:\n")
+        print(Style.RESET_ALL)
+    # Generating a list of markers for given number of players
+    for n in range(int(total_players) + 1):
+        player_markers.append(n + 1)
+    # User input for field size and validation of given input
+    field_size = input(f"Please enter field size(must be greater than or equals to {total_players}):\n")
+    while not field_size.isdigit():
+        field_size = input(Fore.RED + "Please enter valid field size:\n")
+        print(Style.RESET_ALL)
+    while field_size < total_players:
+        field_size = input(Fore.RED + f"Field size must be bigger or equals to {total_players}:\n")
+        print(Style.RESET_ALL)
+    field_size = int(field_size)
+    while field_size <= 1:
+        field_size = input(Fore.RED + "Please enter valid field size bigger than 1:\n")
+        print(Style.RESET_ALL)
+    # Generating the play field
+    for i in range(field_size):
+        field.append([])
+        [field[i].append("-") for _ in range(field_size)]
+    # Player move input, validating given input and updating the field
+    while True:
         print_matrix(field)
-        try:
-            player_one_move = input("Player one, please enter coordinates for your move:\n")
-            if not is_move_valid(player_one_move, field_size):
-                print("Invalid move, please enter unoccupied coordinates within the field: \n")
-                continue
-            row, col = int(player_one_move.split(" ")[0]) - 1, int(player_one_move.split(" ")[1]) - 1
-            field[row][col] = "X"
-            player_one_turn = False
-        except IndexError:
-            print("Invalid move, please enter unoccupied coordinates within the field: \n")
-        except ValueError:
-            print("Invalid move, please enter unoccupied coordinates within the field: \n")
+        player_one_move = input(f"Player {current_player + 1}, please enter coordinates for your move:\n")
+        if not is_move_valid(player_one_move, field_size, field):
+            print(Fore.RED + "Invalid move, please enter unoccupied coordinates within the field: \n")
+            print(Style.RESET_ALL)
+            continue
+        row, col = player_one_move.split(" ")
+        row = int(row) - 1
+        col = int(col) - 1
+        field[row][col] = player_markers[current_player]
+        # Checking if current player is winner
+        if is_player_winner(field):
+            print(Back.GREEN + f"Player {current_player + 1} is winner")
+            print_matrix(field)
+            exit(0)
+        # Checking if result is draw - no empty spaces left and current player is not winner
+        if is_field_full(field):
+            print(Back.GREEN + "Result is draw")
+            print_matrix(field)
+            exit(0)
+        # Switching to next player if current is not winner and result isn't draw
+        current_player += 1
+        if current_player > int(total_players) - 1:
+            current_player = 0
 
-    if is_player_winner(field):
-        exit("Player one is winner.")
-    if is_result_draw(field):
-        exit("Result is draw")
-
-    while not player_one_turn:
-        print_matrix(field)
-        try:
-            player_two_move = input("Player two, please enter coordinates for your move:\n")
-            if not is_move_valid(player_two_move, field_size):
-                print("Invalid move, please enter unoccupied coordinates within the field: \n")
-                continue
-            row, col = int(player_two_move.split(" ")[0]) - 1, int(player_two_move.split(" ")[1]) - 1
-            field[row][col] = "O"
-            player_one_turn = True
-        except IndexError:
-            print("Invalid move, please enter unoccupied coordinates within the field: \n")
-        except ValueError:
-            print("Invalid move, please enter unoccupied coordinates within the field: \n")
-
-    if is_player_winner(field):
-        exit("Player two is winner.")
-    if is_result_draw(field):
-        exit("Result is draw")
